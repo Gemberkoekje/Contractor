71c012bb2b64064206f61ca2cc409c3020b29213
Arie Hofland <Arie_Hofland@hotmail.com>
2019-01-30 20:43:46 +0100

    bla

--- ContractorCore/Class1.cs
+++ ContractorCore/Class1.cs
@@ -6,53 +6,34 @@ using System.Diagnostics;
 
 namespace ContractorCore
 {
-    public enum LocationType
-    {
-        Continent,
-        Planet,
-        SolarSystem,
-        MilkyWay
-    }
-
-    public class Location
-    {
-        public ObjectId _id;
-        public Location ParentLocation;
-
-        public string Name;
-        public LocationType LocationType;
-
-    }
-
-    public class Contractor
-    {
-        public ObjectId _id;
-        public string Name;
-        public Location Location;
-    }
-
     public class Class1
     {
         public Class1()
         {
-            var client = new MongoClient("mongodb://localhost");
-            var database = client.GetDatabase("ContractorDb");
             var map1 = BsonClassMap.RegisterClassMap<Location>();
             var map2 = BsonClassMap.RegisterClassMap<Contractor>();
 
-            var collection = database.GetCollection<Contractor>("Contractors");
-
-            var contractor = new Contractor() { Name = "Test", Location = new Location() { Name = "Europe", LocationType = LocationType.Continent } };
+            var collection = DataProvider.GetDatabase().GetCollection<Contractor>("Contractors");
+            var location = new Location()
+            {
+                Name = "Europe",
+                LocationType = LocationType.Continent
+            };
+            var test = new MongoDbRef<Location>();
+            test.Set(location);
+            var contractor = new Contractor() { Name = "Test", Location = test };
             try
             {
                 collection.InsertOne(contractor);
-            }catch (Exception e)
+                foreach (var document in collection.Find(new BsonDocument()).ToList())
+                {
+                    Console.WriteLine(document.ToBsonDocument().ToString());
+                    Console.WriteLine(document.Location.Get().ToBsonDocument().ToString());
+                }
+            }
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
-            }
-            foreach (var document in collection.Find(new BsonDocument()).ToList())
-            {
-                Console.WriteLine(document.ToBsonDocument().ToString());
             }
         }
     }
